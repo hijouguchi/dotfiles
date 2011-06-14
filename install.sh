f70daf4692decebc543f338dfe069c15b7e18869
hijouguchi <taka13.mac@gmail.com>
2011-06-14 09:45:24 +0900

    create keymap.screenrc and fixed install.sh

--- install.sh
+++ install.sh
@@ -1,33 +1,13 @@
 #!/usr/bin/env sh
 # vim: ft=sh: et
 
-if [ ! -f ~/.irbrc ]; then
-  ln -sf dotfiles/.irbrc ~/.irbrc
-  echo installed .irbrc
-fi
-if [ ! -f ~/.gitconfig ]; then
-  ln -sf dotfiles/.gitconfig ~/.gitconfig
-  echo installed .gitconfig
-fi
-if [ ! -f ~/.vimrc ]; then
-  ln -sf dotfiles/.vimrc ~/.vimrc
-  echo installed .vimrc
-fi
-if [ ! -f ~/.zlogin ]; then
-  ln -sf dotfiles/.zlogin ~/.zlogin
-  echo installed .zlogin
-fi
-if [ ! -f ~/.zshrc ]; then
-  ln -sf dotfiles/.zshrc ~/.zshrc
-  echo installed .zshrc
-fi
-if [ ! -f ~/.screenrc ]; then
-  ln -sf dotfiles/.screenrc ~/.screenrc
-  echo installed .screenrc
-fi
-if [ ! -f ~/.vim ]; then
-  ln -sf dotfiles/.vim ~/.vim
-  echo installed .vim
-fi
 
-echo prease export fpath
+[[ ! -f $HOME/.irbrc ]]                   && ln -sf $HOME/dotfiles/.irbrc     $HOME/.irbrc
+[[ ! -f $HOME/.gitconfig ]]               && ln -sf $HOME/dotfiles/.gitconfig $HOME/.gitconfig
+[[ ! -f $HOME/.vimrc ]]                   && ln -sf $HOME/dotfiles/.vimrc     $HOME/.vimrc
+[[ ! -f $HOME/.vim ]]                     && ln -sf $HOME/dotfiles/.vim       $HOME/.vim
+[[ ! -f $HOME/.zlogin ]]                  && ln -sf $HOME/dotfiles/.zlogin    $HOME/.zlogin
+[[ ! -f $HOME/.zshrc ]]                   && ln -sf $HOME/dotfiles/.zshrc     $HOME/.zshrc
+[[ ! -f $HOME/.screenrc ]]                && ln -sf $HOME/dotfiles/.screenrc  $HOME/.screenrc
+[[ ! -f $HOME/.zshenv ]]                  && echo 'export FPATH=$HOME/dotfiles/zfunctions:$FPATH' >> $HOME/.zshenv
+[[ ! -f $HOME/dotfiles/keymap.screenrc ]] && echo 'escape ^Gt' >> $HOME/dotfiles/keymap.screenrc
