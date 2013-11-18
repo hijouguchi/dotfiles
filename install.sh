0263ddc9678990ae64dd2921b376435faabe2c44
hijouguchi <taka13.mac@gmail.com>
2013-11-18 17:32:14 +0900

    fix install.sh

--- install.sh
+++ install.sh
@@ -5,14 +5,17 @@
 mkdir -p $HOME/.history
 
 # dotfile を置く
-[[ ! -f $HOME/.pryrc ]]     && ln -sf $HOME/dotfiles/.pryrc     $HOME/.pryrc
-[[ ! -f $HOME/.gitconfig ]] && ln -sf $HOME/dotfiles/.gitconfig $HOME/.gitconfig
-[[ ! -f $HOME/.vimrc ]]     && ln -sf $HOME/dotfiles/.vimrc     $HOME/.vimrc
-[[ ! -f $HOME/.vim ]]       && ln -sf $HOME/dotfiles/.vim       $HOME/.vim
-[[ ! -f $HOME/.screenrc ]]  && ln -sf $HOME/dotfiles/.screenrc  $HOME/.screenrc
-[[ ! -f $HOME/.zshrc ]]     && ln -sf $HOME/dotfiles/.zshrc     $HOME/.zshrc
-[[ ! -f $HOME/.zshenv ]]    && cp     $HOME/dotfiles/zshenv     $HOME/.zshenv
+cd $HOME
+[[ ! -f $HOME/.pryrc ]]     && ln -sf $HOME/dotfiles/.pryrc     .
+[[ ! -f $HOME/.gitconfig ]] && ln -sf $HOME/dotfiles/.gitconfig .
+[[ ! -f $HOME/.vimrc ]]     && ln -sf $HOME/dotfiles/.vimrc     .
+[[ ! -f $HOME/.vim ]]       && ln -sf $HOME/dotfiles/.vim       .
+[[ ! -f $HOME/.screenrc ]]  && ln -sf $HOME/dotfiles/.screenrc  .
+[[ ! -f $HOME/.zshrc ]]     && ln -sf $HOME/dotfiles/.zshrc     .
+[[ ! -f $HOME/.zshenv ]]    && cp     $HOME/dotfiles/zshenv     .zshenv
 
 # vim の bundle を持ってくる
-dirname $0 # dotfiles に移動
+cd `dirname $0` # dotfiles に移動
 git submodule update --init
+
+echo 'vim: BundleInstall'
